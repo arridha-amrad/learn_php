6c96f4b08e2085d0c88ab34e18b19d423cf42171
Arridha Amrad <arridhaamrad@gmail.com>
2024-12-10 15:25:39 +0700

    create mysql db and pdo first step

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
 
 require "functions.php";
-require "router.php";
+// require "router.php";
+
+// connect to mysql db
+$dsn = "mysql:host=127.0.0.1;port=3306;dbname=learn_php;user=root;charset=utf8mb4";
+// dsn = data source name
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare('select * from posts');
+$statement->execute();
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>{$post['title']}</li>";
+}
