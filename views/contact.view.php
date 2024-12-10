1cc3af77fb8c9dd663892da1f2a6e7a9595199bf
Arridha Amrad <arridhaamrad@gmail.com>
2024-12-10 14:48:42 +0700

    refactor

--- views/contact.view.php
+++ views/contact.view.php
@@ -1,8 +1,8 @@
-<?php require "views/partials/head.php" ?>
-
-<?php require "views/partials/nav.php" ?>
-
-<?php require "views/partials/banner.php" ?>
+<?php
+require "partials/head.php";
+require "partials/nav.php";
+require "partials/banner.php";
+?>
 
 <main>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
@@ -10,4 +10,4 @@
   </div>
 </main>
 
-<?php require "views/partials/footer.php" ?>
+<?php require "partials/footer.php" ?>
