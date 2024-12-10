7299e52f5e4e9113aed5d4fec33f781ab95b190c
Arridha Amrad <arridhaamrad@gmail.com>
2024-12-11 04:09:41 +0700

    form submission and escape untrusted input

--- views/note.views.php
+++ views/note.views.php
@@ -5,16 +5,16 @@ require "partials/banner.php";
 ?>
 
 <main>
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <p class="mb-6">
-            <a class="py-2 px-4 bg-blue-500 rounded text-white" href="/notes">go back</a>
-        </p>
-        <div>
-            <h1 class="text-xl font-semibold">
-                <?= $note["body"] ?>
-            </h1>
-        </div>
-    </div>
+	<div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+		<p class="mb-6">
+			<a class="py-2 px-4 bg-blue-500 rounded text-white" href="/notes">go back</a>
+		</p>
+		<div>
+			<h1 class="text-xl font-semibold">
+				<?= htmlspecialchars($note["body"]) ?>
+			</h1>
+		</div>
+	</div>
 </main>
 
 <?php require "partials/footer.php" ?>
--- views/notes.views.php
+++ views/notes.views.php
@@ -5,15 +5,18 @@ require "partials/banner.php";
 ?>
 
 <main>
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <?php foreach ($notes as $note) : ?>
-            <li>
-                <a class="text-blue-500 hover:underline" href="/note?id=<?= $note['id'] ?>">
-                    <?= $note["body"] ?>
-                </a>
-            </li>
-        <?php endforeach; ?>
-    </div>
+	<div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+		<?php foreach ($notes as $note) : ?>
+			<li>
+				<a class="text-blue-500 hover:underline" href="/note?id=<?= $note['id'] ?>">
+					<?= htmlspecialchars($note["body"]) ?>
+				</a>
+			</li>
+		<?php endforeach; ?>
+		<div class="py-4">
+			<a class="text-blue-500 underline" href="/note/create">Create note</a>
+		</div>
+	</div>
 </main>
 
 <?php require "partials/footer.php" ?>
