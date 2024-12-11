94399363708cf65eefddff4f79c58aef374b897c
arridha-amrad <arridhaamrad@gmail.com>
2024-12-11 18:13:21 +0700

    namespacing

--- Core/Validator.php
+++ Core/Validator.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Core;
+
 class Validator
 {
   public static function string($text, $min = 1, $max = INF)
--- controllers/about.php
+++ controllers/about.php
@@ -1,7 +1,5 @@
 <?php
 
-$heading = "About Us";
-$title = "About";
 
 view("about.views.php", [
   "heading" => "About Us",
